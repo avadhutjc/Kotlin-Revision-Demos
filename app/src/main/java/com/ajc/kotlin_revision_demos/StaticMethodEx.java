bff846089bf49899e7e7dc2cad8999c8edab1cf5
Avadhutjc <Avadhutjc@gmail.com>
2022-01-06 17:03:26 +0530

    committedd

---
+++ app/src/main/java/com/ajc/kotlin_revision_demos/StaticMethodEx.java
@@ -0,0 +1,15 @@
+package com.ajc.kotlin_revision_demos;
+
+public class StaticMethodEx {
+//
+////    public static void FirstMethod() {
+////        System.out.print("Done");
+////    }
+//
+////    public void SecondMethod() {
+////        System.out.println("Not done");
+////    }
+//    public static void main(String[] args) {
+//        System.out.println("HHH");
+//    }
+}
--- app/src/main/java/com/ajc/kotlin_revision_demos/aa.kt
+++ app/src/main/java/com/ajc/kotlin_revision_demos/aa.kt
@@ -12,7 +12,18 @@ Kotlin: Null can not be a value of a non-null type String
 
 fun main() {
     var name: String? = "MindOrks"
-    print(name?.length) // ok
+    println(name) // MindOrks
+    //assigning the variable 1
     name = null
+    println(name?.length) //null
+    //assigning the variable 2
+    name = "Avadhut J"
+    print(name.length)//Unnecessary safe call on a non-null receiver of type String?
 }
+/*
+MindOrks
+null
+9
+Process finished with exit code 0
+ */
 
